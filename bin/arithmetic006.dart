cffc45e26a0b9245dac0c967872d05642d2d2241
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-14 00:06:18 +0500

    fdf

--- bin/arithmetic006.dart
+++ bin/arithmetic006.dart
@@ -1,4 +1,11 @@
+import 'dart:math';
+
 // Create function func with arguments
 // the task condition is in the Readme file
+num func(a, s) {
+  return pow(a, s);
+}
 
-void main() {}
+void main() {
+  print(func(2, 6));
+}
