3aad52106ab6cba8cce8ce1bdfd824e2ab1155fa
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-14 16:43:11 +0500

    rgse

--- bin/arithmetic036.dart
+++ bin/arithmetic036.dart
@@ -1,4 +1,11 @@
 // Create function func with arguments
 // the task condition is in the Readme file
+import 'dart:math';
 
-void main() {}
+num func(a, b) {
+  return 2 * pow(a, b);
+}
+
+void main() {
+  print(func(2, 4));
+}
