2099e718e2f640a959e5f7519a5eac709ab96217
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-14 10:00:07 +0500

    seggf

--- bin/arithmetic019.dart
+++ bin/arithmetic019.dart
@@ -1,4 +1,9 @@
 // Create function func with arguments
 // the task condition is in the Readme file
+double func(a, b) {
+  return ((a * a) + 2 * ((a * a) + (b * b)) / (4 * a));
+}
 
-void main() {}
+void main() {
+  print(func(10, 4));
+}
