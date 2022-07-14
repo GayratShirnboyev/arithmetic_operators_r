2de66685d261e38de7a572d5b674f151c625046a
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-14 10:47:33 +0500

    esrfserf

--- bin/arithmetic022.dart
+++ bin/arithmetic022.dart
@@ -1,4 +1,9 @@
 // Create function func with arguments
 // the task condition is in the Readme file
+double func(a, b, c) {
+  return (2 * (b * b) - a) / ((a * a * a) - (c * c));
+}
 
-void main() {}
+void main() {
+  print(func(2, 4, 6));
+}
